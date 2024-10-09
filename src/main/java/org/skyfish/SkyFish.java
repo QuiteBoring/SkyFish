e63dfcd4b8619346e62aacad1d86137380369375
Nathan <103887913+Nathan-DV@users.noreply.github.com>
2024-10-09 19:59:09 +0000

    Upload Base Code

---
+++ src/main/java/org/skyfish/SkyFish.java
@@ -0,0 +1,15 @@
+package org.skyfish;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "skyfish", useMetadata=true)
+public class SkyFish {
+    
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+}
