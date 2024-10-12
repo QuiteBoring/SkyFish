60149d9838c34b9858c467ad74b9111c36ec1ec2
Nathan <103887913+Nathan-DV@users.noreply.github.com>
2024-10-12 10:03:29 -0400

    Update AutoFlare.java

--- src/main/java/org/skyfish/feature/impl/AutoFlare.java
+++ src/main/java/org/skyfish/feature/impl/AutoFlare.java
@@ -19,11 +19,46 @@ import java.util.stream.Collectors;
 public class AutoFlare extends Feature {
 
     private final Pattern ORB_PATTERN = Pattern.compile("[A-Za-z ]* (?<seconds>[0-9]*)s");
-    private Orb orb = null;
     private Flare flare = null;
+    private boolean running = false;
     
     public AutoFlare() {
         super("AutoFlare");
+    }
+
+    @Override
+    public void start() {
+        flare = null;
+        running = false;
+    }
+
+    @Override 
+    public void stop() {
+        flare = null;
+        running = false;
+    }
+
+    public void placeFlare(Runnable runnable) {
+        if (flare != null || !flare.isDead || !Config.getInstance().FEATURE_AUTO_FLARE || running) return;
+        running = true;
+        
+        Multithreading.runAsync(() -> {
+            try {
+                int slot = InventoryUtils.searchItem("Flare") == -1 ? InventoryUtils.searchItem("Flux") : InventoryUtils.searchItem("Flare");
+
+                if (slot == -1) {
+                    LogUtils.sendDebug("No flux or flare found in hotbar");
+                } else {
+                    Thread.sleep(Config.getInstance().getDelay());
+                    mc.thePlayer.inventory.currentItem = slot;
+                    Thread.sleep(100);        
+                    mc.playerController.sendUseItem(mc.thePlayer, mc.theWorld, mc.thePlayer.heldItem);
+                    Thread.sleep(100);
+                }
+            } catch (Exception error) {}
+            runnable.run();
+            running = false;
+        });
     }
 
     @Override
@@ -38,23 +73,24 @@ public class AutoFlare extends Feature {
             EntityLivingBase as = (EntityLivingBase) armorstand;
             ItemStack head = as.getEquipmentInSlot(4);
             if (head == null || !head.hasTagCompound()) continue;
-            Flare.Type type = getFlareType(head);
+            Type type = getFlareType(head);
             if (type == null || this.flare != null || !this.flare.isDead) continue;
-            if (type == getFlareTypeHotbar()) this.flare = new Flare(armorstand, type);
+            if (type == getTypeHotbar()) this.flare = new Flare(armorstand, type);
         }
     }
     
-    private Flare.Type getFlareType(ItemStack head) {
+    private Type getFlareType(ItemStack head) {
         String[] flareSkins = new String[] { "ewogICJ0aW1lc3RhbXAiIDogMTY0NjY4NzMwNjIyMywKICAicHJvZmlsZUlkIiA6ICI0MWQzYWJjMmQ3NDk0MDBjOTA5MGQ1NDM0ZDAzODMxYiIsCiAgInByb2ZpbGVOYW1lIiA6ICJNZWdha2xvb24iLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjJlMmJmNmMxZWMzMzAyNDc5MjdiYTYzNDc5ZTU4NzJhYzY2YjA2OTAzYzg2YzgyYjUyZGFjOWYxYzk3MTQ1OCIKICAgIH0KICB9Cn0=", 
                                             "ewogICJ0aW1lc3RhbXAiIDogMTY0NjY4NzMyNjQzMiwKICAicHJvZmlsZUlkIiA6ICI0MWQzYWJjMmQ3NDk0MDBjOTA5MGQ1NDM0ZDAzODMxYiIsCiAgInByb2ZpbGVOYW1lIiA6ICJNZWdha2xvb24iLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWQyYmY5ODY0NzIwZDg3ZmQwNmI4NGVmYTgwYjc5NWM0OGVkNTM5YjE2NTIzYzNiMWYxOTkwYjQwYzAwM2Y2YiIKICAgIH0KICB9Cn0=", 
                                             "ewogICJ0aW1lc3RhbXAiIDogMTY0NjY4NzM0NzQ4OSwKICAicHJvZmlsZUlkIiA6ICI0MWQzYWJjMmQ3NDk0MDBjOTA5MGQ1NDM0ZDAzODMxYiIsCiAgInByb2ZpbGVOYW1lIiA6ICJNZWdha2xvb24iLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzAwNjJjYzk4ZWJkYTcyYTZhNGI4OTc4M2FkY2VmMjgxNWI0ODNhMDFkNzNlYTg3YjNkZjc2MDcyYTg5ZDEzYiIKICAgIH0KICB9Cn0=" };
         for (int i = 0; i < flareSkins.length; i++) {
             if (head.hasTagCompound() && head.getTagCompound().toString().contains(flareSkins[i])) {
-                if (i == 0) return Flare.Type.WARNING;
-                if (i == 1) return Flare.Type.ALERT;
-                if (i == 2) return Flare.Type.WARNING;
+                if (i == 0) return Type.WARNING;
+                if (i == 1) return Type.ALERT;
+                if (i == 2) return Type.WARNING;
             }
         }
+        
         return null;
     }
     
@@ -64,7 +100,7 @@ public class AutoFlare extends Feature {
     
         if (entity instanceof EntityArmorStand && entity.hasCustomName()) {
             String nameTag = entity.getCustomNameTag();
-            OrbType orb = OrbType.getByName(nameTag);
+            Type orb = Type.getOrb(nameTag);
             
             if (orb != null && orb.isInRadius(entity.getDistanceSqToEntity(mc.thePlayer))) { 
                 Matcher matcher = ORB_PATTERN.matcher(StringUtils.stripControlCodes(nameTag));
@@ -74,8 +110,8 @@ public class AutoFlare extends Feature {
                     if (!surroundingArmorStands.isEmpty()) {
                         for (EntityArmorStand surroundingArmorStand : surroundingArmorStands) {
                             ItemStack helmet = surroundingArmorStand.getCurrentArmor(3);
-                            if (helmet != null && (this.orb == null || this.orb.isDead) && orb == getOrbTypeHotbar()) {
-                                this.orb = new Orb(surroundingArmorStand, orb);
+                            if (helmet != null && (this.flare == null || this.flare.isDead) && flare == getTypeHotbar()) {
+                                this.flare = new Flare(surroundingArmorStand, orb);
                             }
                         }
                     }
@@ -83,44 +119,25 @@ public class AutoFlare extends Feature {
             }    
         }
     }
-
-    public OrbType getOrbTypeHotbar() {
-        OrbType orbType = null;
-        int slot = InventoryUtils.searchItem("Orb");
+    
+    public Type getTypeHotbar() {
+        Type type = null;
+        int slot = InventoryUtils.searchItem("Orb") == -1 ? InventoryUtils.searchItem("Flare") : InventoryUtils.searchItem("Orb");
 
         if (slot != -1) {
             ItemStack itemStack = mc.thePlayer.inventory.mainInventory[slot];
             String name = StringUtils.stripControlCodes(itemStack.getDisplayName()).toLowerCase();
 
-            if (name.contains("plasmaflux")) orbType = OrbType.PLASMAFLUX;
-            if (name.contains("overflux")) orbType = OrbType.OVERFLUX;
-            if (name.contains("mana flux")) orbType = OrbType.MANA_FLUX;
-            if (name.contains("radiant")) orbType = OrbType.RADIANT;
-        }
+            if (name.contains("plasmaflux")) type = Type.PLASMAFLUX;
+            if (name.contains("overflux")) type = Type.OVERFLUX;
+            if (name.contains("mana flux")) type = Type.MANA_FLUX;
+            if (name.contains("radiant")) type = Type.RADIANT;
+            if (name.contains("sos")) type = Type.SOS;
+            if (name.contains("alert")) type = Type.ALERT;
+            if (name.contains("warning")) type = Type.WARNING;
+        } 
 
         return orbType;
-    }
-
-    public Flare.Type getFlareTypeHotbar() {
-        Flare.Type flareType = null;
-        int slot = InventoryUtils.searchItem("Flare");
-
-        if (slot != -1) {
-            ItemStack itemStack = mc.thePlayer.inventory.mainInventory[slot];
-            String name = StringUtils.stripControlCodes(itemStack.getDisplayName()).toLowerCase();
-
-            if (name.contains("sos")) flareType = Flare.Type.SOS;
-            if (name.contains("alert")) flareType = Flare.Type.ALERT;
-            if (name.contains("warning")) flareType = Flare.Type.WARNING;
-        }
-
-        return flareType;
-    }
-    
-    public boolean shouldPlace() {
-        if (getOrbTypeHotbar() != null && (orb == null || orb.isDead)) return true;
-        if (getFlareTypeHotbar() != null && (flare == null || flare.isDead)) return true;
-        return false;
     }
     
     private static AutoFlare instance;
@@ -132,34 +149,29 @@ public class AutoFlare extends Feature {
         return instance;
     }
 
-    private class Orb {
+    private class Flare {
         public final EntityArmorStand entity;
-        public final OrbType type;
+        public final Type type;
         
-        public Orb(EntityArmorStand entity, OrbType type) {
+        public Flare(EntityArmorStand entity, Type type) {
             this.entity = entity;
             this.type = type;
         }
-
-        public boolean isBetterThan(OrbType type) {
-            if (this.type.priority < type.priority) {
-                return true;
-            }
-
-            return false;
-        }
     }
         
-    private static enum OrbType {
+    private static enum Type {
         RADIANT("§aRadiant", 18*18),
         MANA_FLUX("§9Mana Flux", 18*18),
         OVERFLUX("§5Overflux", 18*18),
-        PLASMAFLUX("§d§lPlasmaflux", 20*20);
+        PLASMAFLUX("§d§lPlasmaflux", 20*20),
+        SOS("SOS", -1),
+        Alert("Alert", -1),
+        Warning("Warning", -1);
 
         private String display;
         private int rangeSquared;
         
-        private OrbType(String display, int rangeSquared) {
+        private Type(String display, int rangeSquared) {
             this.display = display;
             this.rangeSquared = rangeSquared;
         }    
@@ -168,29 +180,13 @@ public class AutoFlare extends Feature {
             return distanceSquared <= rangeSquared;
         }
 
-        public static OrbType getByName(String name) {
-            for (OrbType orb : values()) {
+        public static Type getOrb(String name) {
+            for (Type orb : values()) {
                 if (name.startsWith(orb.display)) {
                     return orb;
                 }
             }
             return null;
-        }
-    }
-
-    private class Flare {
-        public final EntityArmorStand entity;
-        public final Flare.Type type;
-        
-        public Flare(EntityArmorStand entity, Flare.Type type) {
-            this.entity = entity;
-            this.type = type;
-        }
-        
-        private enum Type {
-            SOS,
-            ALERT,
-            WARNING;
         }
     }
 
