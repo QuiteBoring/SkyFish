c164dff781f6485dd063a50c47ab0d3a8877e2e2
Nathan <103887913+Nathan-DV@users.noreply.github.com>
2024-10-15 06:52:30 -0400

    Update DisconnectFailsafe.java

--- src/main/java/org/skyfish/failsafe/impl/DisconnectFailsafe.java
+++ src/main/java/org/skyfish/failsafe/impl/DisconnectFailsafe.java
@@ -1,6 +1,5 @@
 package org.skyfish.failsafe.impl;
 
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 import org.skyfish.failsafe.*;
 import org.skyfish.util.Config;
@@ -11,7 +10,7 @@ public class DisconnectFailsafe extends Failsafe {
         super(Failsafe.Type.DISCONNECT);
     }
 
-    @SubscribeEvent
+    @Override
     public void onDisconnect(FMLNetworkEvent.ClientDisconnectionFromServerEvent event) {
         if (!Config.getInstance().FAILSAFE_DISCONNECT) return;
         FailsafeManager.getInstance().possibleDetection(this);
